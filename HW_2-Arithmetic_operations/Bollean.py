57f981b35d5543e28e89230219b810a020cf0d62
Pavlik1100 <simonovpavlik@gmail.com>
2021-12-12 15:23:58 +0300

    added README.md Bollean.py, arithmetic.py, arithmetic_2.py

---
+++ HW_2-Arithmetic_operations/Bollean.py
@@ -0,0 +1,30 @@
+# Boolean
+#
+#  48. Создать переменную b_item_t и присвоить True
+b_item_t = True
+#  49. Создать переменную b_item_f и присвоить False
+b_item_f = False
+#  50. Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f
+b_item_result_sum = b_item_f + b_item_t
+#  51. Вывести b_item_relult_sum в консоль.
+print('b_item_reslt_sum =', b_item_result_sum)
+#  52. Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f
+b_item_result_subtr = b_item_t - b_item_f
+#  53. Вывести b_item_relult_subtr в консоль.
+print('b_item_result_subtr =', b_item_result_subtr)
+#  54. Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f
+b_item_result_multi = b_item_t * b_item_f
+#  55. Вывести b_item_relult_multi в консоль.
+print('b_item_result_multi =', b_item_result_multi)
+#  56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
+#  b_item_result_decision = b_item_t / b_item_f
+#  57. Вывести b_item_relult_division в консоль. (Получить ошибку)
+#  print('b_item_result_decision', b_item_result_decision)
+#  58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
+b_item_1_int = int(b_item_t)
+#  59. Вывести b_item_1_int в консоль.
+print('b_item_1_int =', b_item_1_int)
+#  60. Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int
+b_item_2_int = int(b_item_f)
+#  61. Вывести b_item_2_int в консоль.
+print('b_item_2_int =', b_item_2_int)
